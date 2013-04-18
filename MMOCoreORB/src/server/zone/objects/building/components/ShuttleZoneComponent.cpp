swgemu/Core3

[Changed] Starport wait times to 1 minute - mantis 3551 [Adjusted] Duration of "about to begin boarding" for shuttleports and starports to more closely match their respective animations
Change-Id: Icb4a9cf109d89e323d931de88a1b8a5a3ad58070

MMOCoreORB/src/server/zone/objects/building/components/ShuttleZoneComponent.cpp
@@ -37,7 +37,7 @@ void ShuttleZoneComponent::notifyInsertToZone(SceneObject* sceneObject, Zone* zo
 			PlanetTravelPoint* planetTravelPoint = new PlanetTravelPoint(zoneName, cityRegion->getRegionName(), arrivalVector, arrivalVector, shuttle);
 			zone->getPlanetManager()->addPlayerCityTravelPoint(planetTravelPoint);
 			cityRegion->setShuttleID(shuttle->getObjectID());
-			zone->getPlanetManager()->scheduleShuttle(shuttle);
+			zone->getPlanetManager()->scheduleShuttle(shuttle, ShuttleDepartureTask::SHUTTLEPORT);
 		}
 	}
 
@@ -48,7 +48,11 @@ void ShuttleZoneComponent::notifyInsertToZone(SceneObject* sceneObject, Zone* zo
 		Reference<PlanetTravelPoint*> ptp = planetManager->getNearestPlanetTravelPoint(shuttle, 128.f);
 
 		if (ptp != NULL) {
-			planetManager->scheduleShuttle(shuttle);
+			if (ptp->isInterplanetary())
+				planetManager->scheduleShuttle(shuttle, ShuttleDepartureTask::STARPORT);
+			else
+				planetManager->scheduleShuttle(shuttle, ShuttleDepartureTask::SHUTTLEPORT);
+
 			ptp->setShuttle(shuttle);
 		}
 	}

MMOCoreORB/src/server/zone/objects/region/CityRegionImplementation.cpp
@@ -96,7 +96,7 @@ void CityRegionImplementation::notifyLoadFromDatabase() {
 		zone->getPlanetManager()->addPlayerCityTravelPoint(planetTravelPoint);
 
 		if (shuttle != NULL) {
-			zone->getPlanetManager()->scheduleShuttle(shuttle);
+			zone->getPlanetManager()->scheduleShuttle(shuttle, ShuttleDepartureTask::SHUTTLEPORT);
 			zone->registerObjectWithPlanetaryMap(zone->getZoneServer()->getObject(shuttleID, false));
 		}
 	}

Tatwi/legend-of-hondo-classic

MMOCoreORB/src/server/zone/objects/building/components/ShuttleZoneComponent.cpp
@@ -37,7 +37,7 @@ void ShuttleZoneComponent::notifyInsertToZone(SceneObject* sceneObject, Zone* zo
 			PlanetTravelPoint* planetTravelPoint = new PlanetTravelPoint(zoneName, cityRegion->getRegionName(), arrivalVector, arrivalVector, shuttle);
 			zone->getPlanetManager()->addPlayerCityTravelPoint(planetTravelPoint);
 			cityRegion->setShuttleID(shuttle->getObjectID());
-			zone->getPlanetManager()->scheduleShuttle(shuttle);
+			zone->getPlanetManager()->scheduleShuttle(shuttle, PlanetManager::SHUTTLEPORT);
 		}
 	}
 
@@ -48,7 +48,11 @@ void ShuttleZoneComponent::notifyInsertToZone(SceneObject* sceneObject, Zone* zo
 		Reference<PlanetTravelPoint*> ptp = planetManager->getNearestPlanetTravelPoint(shuttle, 128.f);
 
 		if (ptp != NULL) {
-			planetManager->scheduleShuttle(shuttle);
+			if (ptp->isInterplanetary())
+				planetManager->scheduleShuttle(shuttle, PlanetManager::STARPORT);
+			else
+				planetManager->scheduleShuttle(shuttle, PlanetManager::SHUTTLEPORT);
+
 			ptp->setShuttle(shuttle);
 		}
 	}

MMOCoreORB/src/server/zone/objects/region/CityRegionImplementation.cpp
@@ -96,7 +96,7 @@ void CityRegionImplementation::notifyLoadFromDatabase() {
 		zone->getPlanetManager()->addPlayerCityTravelPoint(planetTravelPoint);
 
 		if (shuttle != NULL) {
-			zone->getPlanetManager()->scheduleShuttle(shuttle);
+			zone->getPlanetManager()->scheduleShuttle(shuttle, PlanetManager::SHUTTLEPORT);
 			zone->registerObjectWithPlanetaryMap(zone->getZoneServer()->getObject(shuttleID, false));
 		}
 	}