swgemu/Core3

[Added] ability to rename guilds (delay is accelerated for testing)
Change-Id: Ia413c71e6ac8ca67e72bf3d3e216fa0f9dcb81c9

MMOCoreORB/src/server/zone/objects/guild/RenameGuildTask.h
@@ -1,0 +1,55 @@
+
+#ifndef RENAMEGUILDTASK_H_
+#define RENAMEGUILDTASK_H_
+
+#include "engine/engine.h"
+#include "server/zone/objects/guild/GuildObject.h"
+#include "server/zone/ZoneServer.h"
+
+class RenameGuildTask : public Task {
+	ZoneServer* server;
+
+	ManagedWeakReference<GuildObject*> guildObject;
+
+	ManagedWeakReference<CreatureObject*> member;
+
+public:
+	RenameGuildTask(CreatureObject* creature, ZoneServer* zserv, GuildObject* guildObj) : Task() {
+		guildObject = guildObj;
+
+		server = zserv;
+
+		member = creature;
+	}
+
+	void run() {
+		ManagedReference<GuildObject*> guild = guildObject.get();
+
+		if (guild == NULL || !guild->isRenamePending())
+			return;
+
+		ManagedReference<CreatureObject*> player = member.get();
+		ManagedReference<GuildManager*> guildManager = server->getGuildManager();
+
+		if (player == NULL || guildManager == NULL)
+			return;
+
+		String newName = guild->getPendingNewName();
+		String newAbbrev = guild->getPendingNewAbbrev();
+
+		if (!guildManager->validateGuildRename(player, newName, guild)) {
+			guild->setRenamePending(false);
+			return;
+		}
+
+		if (!guildManager->validateGuildRenameAbbrev(player, newAbbrev, guild)) {
+			guild->setRenamePending(false);
+			return;
+		}
+
+		guildManager->renameGuild(guild, newName, newAbbrev);
+	}
+};
+
+
+#endif /* RENAMEGUILDTASK_H_ */

MMOCoreORB/src/server/zone/objects/guild/sui/GuildChangeAbbrevResponseSuiCallback.h
@@ -1,0 +1,78 @@
+
+#ifndef GUILDCHANGEABBREVRESPONSESUICALLBACK_H_
+#define GUILDCHANGEABBREVRESPONSESUICALLBACK_H_
+
+#include "server/zone/managers/guild/GuildManager.h"
+#include "server/zone/objects/tangible/terminal/guild/GuildTerminal.h"
+#include "server/zone/objects/player/sui/SuiCallback.h"
+
+class GuildChangeAbbrevResponseSuiCallback : public SuiCallback {
+	ManagedWeakReference<GuildObject*> guildObject;
+
+public:
+	GuildChangeAbbrevResponseSuiCallback(ZoneServer* server, GuildObject* guild)
+		: SuiCallback(server) {
+
+		guildObject = guild;
+	}
+
+	void run(CreatureObject* player, SuiBox* suiBox, bool cancelPressed, Vector<UnicodeString>* args) {
+		uint64 playerID = player->getObjectID();
+
+		ManagedReference<GuildManager*> guildManager = server->getGuildManager();
+
+		ManagedReference<GuildObject*> guild = guildObject.get();
+
+		//If a rename isn't pending, then exit.
+		if (!guild->isRenamePending())
+			return;
+
+		if (!guild->hasNamePermission(playerID) && !player->getPlayerObject()->isPrivileged()) {
+			player->sendSystemMessage("@guild:generic_fail_no_permission"); // You do not have permission to perform that operation.
+			return;
+		}
+
+		//After this point, we have to setRenamePending(false) anywhere we return, since they have to be renaming a guild at this point.
+
+		if (!suiBox->isInputBox() || cancelPressed) {
+			guild->setRenamePending(false);
+			return;
+		}
+
+		if (args->size() < 1) {
+			guild->setRenamePending(false);
+			return;
+		}
+
+		String guildAbbrev = args->get(0).toString();
+
+		ManagedReference<SceneObject*> obj = suiBox->getUsingObject();
+
+		if (obj == NULL || !obj->isTerminal()) {
+			guild->setRenamePending(false);
+			return;
+		}
+
+		Terminal* terminal = cast<Terminal*>( obj.get());
+
+		if (!terminal->isGuildTerminal()) {
+			guild->setRenamePending(false);
+			return;
+		}
+
+		GuildTerminal* guildTerminal = cast<GuildTerminal*>( terminal);
+
+		if (guildManager->validateGuildRenameAbbrev(player, guildAbbrev, guild)) {
+			guild->setPendingNewAbbrev(guildAbbrev);
+			guildManager->scheduleGuildRename(player, guild);
+			return;
+		}
+
+		//Resend the create abbrev box.
+		player->getPlayerObject()->addSuiBox(suiBox);
+		player->sendMessage(suiBox->generateMessage());
+	}
+};
+
+
+#endif /* GUILDCHANGEABBREVRESPONSESUICALLBACK_H_ */

MMOCoreORB/src/server/zone/objects/guild/sui/GuildChangeNameResponseSuiCallback.h
@@ -1,0 +1,72 @@
+
+#ifndef GUILDCHANGENAMERESPONSESUICALLBACK_H_
+#define GUILDCHANGENAMERESPONSESUICALLBACK_H_
+
+#include "server/zone/managers/guild/GuildManager.h"
+#include "server/zone/objects/tangible/terminal/guild/GuildTerminal.h"
+#include "server/zone/objects/player/sui/SuiCallback.h"
+#include "server/zone/objects/player/PlayerObject.h"
+
+class GuildChangeNameResponseSuiCallback : public SuiCallback {
+	ManagedWeakReference<GuildObject*> guildObject;
+
+public:
+	GuildChangeNameResponseSuiCallback(ZoneServer* server, GuildObject* guild)
+		: SuiCallback(server) {
+
+		guildObject = guild;
+	}
+
+	void run(CreatureObject* player, SuiBox* suiBox, bool cancelPressed, Vector<UnicodeString>* args) {
+		uint64 playerID = player->getObjectID();
+
+		ManagedReference<GuildObject*> guild = guildObject.get();
+
+		if (!guild->hasNamePermission(playerID) && !player->getPlayerObject()->isPrivileged()) {
+			player->sendSystemMessage("@guild:generic_fail_no_permission"); // You do not have permission to perform that operation.
+			return;
+		}
+
+		if (!suiBox->isInputBox() || cancelPressed)
+			return;
+
+		if (args->size() < 1)
+			return;
+
+		String guildName = args->get(0).toString();
+
+		ManagedReference<SceneObject*> obj = suiBox->getUsingObject();
+
+		if (obj == NULL || !obj->isTerminal())
+			return;
+
+		Terminal* terminal = cast<Terminal*>( obj.get());
+
+		if (!terminal->isGuildTerminal())
+			return;
+
+		GuildTerminal* guildTerminal = cast<GuildTerminal*>( terminal);
+
+		//Check to make sure the terminal is tied to the guild
+		if (guildTerminal->getGuildObject() != guild)
+			return;
+
+		ManagedReference<GuildManager*> guildManager = server->getGuildManager();
+
+		//Check if this player is already creating a guild...
+		if (guildManager->isCreatingGuild(playerID))
+			return;
+
+		if (guildManager->validateGuildRename(player, guildName, guild)) {
+			guild->setPendingNewName(guildName);
+			guildManager->sendGuildChangeAbbrevTo(player, guild, guildTerminal);
+			return;
+		}
+
+		//Resend the create name box.
+		player->getPlayerObject()->addSuiBox(suiBox);
+		player->sendMessage(suiBox->generateMessage());
+	}
+};
+
+#endif /* GUILDCHANGENAMERESPONSESUICALLBACK_H_ */

Tatwi/legend-of-hondo-classic

MMOCoreORB/src/server/zone/objects/guild/RenameGuildTask.h
@@ -1,0 +1,55 @@
+
+#ifndef RENAMEGUILDTASK_H_
+#define RENAMEGUILDTASK_H_
+
+#include "engine/engine.h"
+#include "server/zone/objects/guild/GuildObject.h"
+#include "server/zone/ZoneServer.h"
+
+class RenameGuildTask : public Task {
+	ZoneServer* server;
+
+	ManagedWeakReference<GuildObject*> guildObject;
+
+	ManagedWeakReference<CreatureObject*> member;
+
+public:
+	RenameGuildTask(CreatureObject* creature, ZoneServer* zserv, GuildObject* guildObj) : Task() {
+		guildObject = guildObj;
+
+		server = zserv;
+
+		member = creature;
+	}
+
+	void run() {
+		ManagedReference<GuildObject*> guild = guildObject.get();
+
+		if (guild == NULL || !guild->isRenamePending())
+			return;
+
+		ManagedReference<CreatureObject*> player = member.get();
+		ManagedReference<GuildManager*> guildManager = server->getGuildManager();
+
+		if (player == NULL || guildManager == NULL)
+			return;
+
+		String newName = guild->getPendingNewName();
+		String newAbbrev = guild->getPendingNewAbbrev();
+
+		if (!guildManager->validateGuildName(player, newName, guild)) {
+			guild->setRenamePending(false);
+			return;
+		}
+
+		if (!guildManager->validateGuildAbbrev(player, newAbbrev, guild)) {
+			guild->setRenamePending(false);
+			return;
+		}
+
+		guildManager->renameGuild(guild, newName, newAbbrev);
+	}
+};
+
+
+#endif /* RENAMEGUILDTASK_H_ */

MMOCoreORB/src/server/zone/objects/guild/sui/GuildChangeAbbrevResponseSuiCallback.h
@@ -1,0 +1,80 @@
+
+#ifndef GUILDCHANGEABBREVRESPONSESUICALLBACK_H_
+#define GUILDCHANGEABBREVRESPONSESUICALLBACK_H_
+
+#include "server/zone/managers/guild/GuildManager.h"
+#include "server/zone/objects/tangible/terminal/guild/GuildTerminal.h"
+#include "server/zone/objects/player/sui/SuiCallback.h"
+
+class GuildChangeAbbrevResponseSuiCallback : public SuiCallback {
+	ManagedWeakReference<GuildObject*> guildObject;
+
+public:
+	GuildChangeAbbrevResponseSuiCallback(ZoneServer* server, GuildObject* guild)
+		: SuiCallback(server) {
+
+		guildObject = guild;
+	}
+
+	void run(CreatureObject* player, SuiBox* suiBox, bool cancelPressed, Vector<UnicodeString>* args) {
+		uint64 playerID = player->getObjectID();
+
+		ManagedReference<GuildManager*> guildManager = server->getGuildManager();
+
+		ManagedReference<GuildObject*> guild = guildObject.get();
+
+		Locker glocker(guild, player);
+
+		//If a rename isn't pending, then exit.
+		if (!guild->isRenamePending())
+			return;
+
+		if (!guild->hasNamePermission(playerID) && !player->getPlayerObject()->isPrivileged()) {
+			player->sendSystemMessage("@guild:generic_fail_no_permission"); // You do not have permission to perform that operation.
+			return;
+		}
+
+		//After this point, we have to setRenamePending(false) anywhere we return, since they have to be renaming a guild at this point.
+
+		if (!suiBox->isInputBox() || cancelPressed) {
+			guild->setRenamePending(false);
+			return;
+		}
+
+		if (args->size() < 1) {
+			guild->setRenamePending(false);
+			return;
+		}
+
+		String guildAbbrev = args->get(0).toString();
+
+		ManagedReference<SceneObject*> obj = suiBox->getUsingObject();
+
+		if (obj == NULL || !obj->isTerminal()) {
+			guild->setRenamePending(false);
+			return;
+		}
+
+		Terminal* terminal = cast<Terminal*>( obj.get());
+
+		if (!terminal->isGuildTerminal()) {
+			guild->setRenamePending(false);
+			return;
+		}
+
+		GuildTerminal* guildTerminal = cast<GuildTerminal*>( terminal);
+
+		if (guildManager->validateGuildAbbrev(player, guildAbbrev, guild)) {
+			guild->setPendingNewAbbrev(guildAbbrev);
+			guildManager->scheduleGuildRename(player, guild);
+			return;
+		}
+
+		//Resend the create abbrev box.
+		player->getPlayerObject()->addSuiBox(suiBox);
+		player->sendMessage(suiBox->generateMessage());
+	}
+};
+
+
+#endif /* GUILDCHANGEABBREVRESPONSESUICALLBACK_H_ */

MMOCoreORB/src/server/zone/objects/guild/sui/GuildChangeNameResponseSuiCallback.h
@@ -1,0 +1,74 @@
+
+#ifndef GUILDCHANGENAMERESPONSESUICALLBACK_H_
+#define GUILDCHANGENAMERESPONSESUICALLBACK_H_
+
+#include "server/zone/managers/guild/GuildManager.h"
+#include "server/zone/objects/tangible/terminal/guild/GuildTerminal.h"
+#include "server/zone/objects/player/sui/SuiCallback.h"
+#include "server/zone/objects/player/PlayerObject.h"
+
+class GuildChangeNameResponseSuiCallback : public SuiCallback {
+	ManagedWeakReference<GuildObject*> guildObject;
+
+public:
+	GuildChangeNameResponseSuiCallback(ZoneServer* server, GuildObject* guild)
+		: SuiCallback(server) {
+
+		guildObject = guild;
+	}
+
+	void run(CreatureObject* player, SuiBox* suiBox, bool cancelPressed, Vector<UnicodeString>* args) {
+		uint64 playerID = player->getObjectID();
+
+		ManagedReference<GuildObject*> guild = guildObject.get();
+
+		if (!guild->hasNamePermission(playerID) && !player->getPlayerObject()->isPrivileged()) {
+			player->sendSystemMessage("@guild:generic_fail_no_permission"); // You do not have permission to perform that operation.
+			return;
+		}
+
+		if (!suiBox->isInputBox() || cancelPressed)
+			return;
+
+		if (args->size() < 1)
+			return;
+
+		String guildName = args->get(0).toString();
+
+		ManagedReference<SceneObject*> obj = suiBox->getUsingObject();
+
+		if (obj == NULL || !obj->isTerminal())
+			return;
+
+		Terminal* terminal = cast<Terminal*>( obj.get());
+
+		if (!terminal->isGuildTerminal())
+			return;
+
+		GuildTerminal* guildTerminal = cast<GuildTerminal*>( terminal);
+
+		//Check to make sure the terminal is tied to the guild
+		if (guildTerminal->getGuildObject() != guild)
+			return;
+
+		ManagedReference<GuildManager*> guildManager = server->getGuildManager();
+
+		//Check if this player is already creating a guild...
+		if (guildManager->isCreatingGuild(playerID))
+			return;
+
+		if (guildManager->validateGuildName(player, guildName, guild)) {
+			Locker glocker(guild, player);
+
+			guild->setPendingNewName(guildName);
+			guildManager->sendGuildChangeAbbrevTo(player, guild, guildTerminal);
+			return;
+		}
+
+		//Resend the create name box.
+		player->getPlayerObject()->addSuiBox(suiBox);
+		player->sendMessage(suiBox->generateMessage());
+	}
+};
+
+#endif /* GUILDCHANGENAMERESPONSESUICALLBACK_H_ */