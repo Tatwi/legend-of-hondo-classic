swgemu/Core3

[added] Humanoid missions to terminals, round 2 (Rori, Talus, Tatooine, Yavin 4) [added] All new Rebel/Imperial/Neutral missions to faction mission terminals
Change-Id: I7018aebe700f94e2ffba2b0e7e2f6dd389b262f5

MMOCoreORB/bin/scripts/mobile/lair/npc_theater/global_bounty_hunter_camp_neutral_small_theater.lua
@@ -6,6 +6,7 @@ global_bounty_hunter_camp_neutral_small_theater = Lair:new {
 	buildingsMedium = {"object/building/poi/anywhere_misc_camp_small_1.iff"},
 	buildingsHard = {"object/building/poi/anywhere_misc_camp_small_1.iff"},
 	buildingsVeryHard = {"object/building/poi/anywhere_misc_camp_small_1.iff"},
+	missionBuilding = "object/tangible/lair/base/objective_banner_generic2.iff",
 	mobType = "npc",
 	buildingType = "theater"
 }

MMOCoreORB/bin/scripts/mobile/lair/npc_theater/global_brigand_highwayman_camp_neutral_large_theater.lua
@@ -6,6 +6,7 @@ global_brigand_highwayman_camp_neutral_large_theater = Lair:new {
 	buildingsMedium =  {"object/building/poi/tatooine_swoop_large1.iff","object/building/poi/tatooine_swoop_large2.iff","object/building/poi/tatooine_swoop_large3.iff","object/building/poi/tatooine_swoop_large4.iff"},
 	buildingsHard =  {"object/building/poi/tatooine_swoop_large1.iff","object/building/poi/tatooine_swoop_large2.iff","object/building/poi/tatooine_swoop_large3.iff","object/building/poi/tatooine_swoop_large4.iff"},
 	buildingsVeryHard = {"object/building/poi/tatooine_swoop_large1.iff","object/building/poi/tatooine_swoop_large2.iff","object/building/poi/tatooine_swoop_large3.iff","object/building/poi/tatooine_swoop_large4.iff"},
+	missionBuilding = "object/tangible/lair/base/objective_banner_generic2.iff",
 	mobType = "npc",
 	buildingType = "theater"
 }

MMOCoreORB/bin/scripts/mobile/lair/npc_theater/global_miner_camp_neutral_small_theater.lua
@@ -6,6 +6,7 @@ global_miner_camp_neutral_small_theater = Lair:new {
 	buildingsMedium = {"object/building/poi/tatooine_mineral_farmer_medium1.iff","object/building/poi/tatooine_mineral_farmer_medium2.iff"},
 	buildingsHard = {"object/building/poi/tatooine_mineral_farmer_medium1.iff","object/building/poi/tatooine_mineral_farmer_medium2.iff"},
 	buildingsVeryHard = {"object/building/poi/tatooine_mineral_farmer_medium1.iff","object/building/poi/tatooine_mineral_farmer_medium2.iff"},
+	missionBuilding = "object/tangible/lair/base/objective_banner_generic2.iff",
 	mobType = "npc",
 	buildingType = "theater"
 }

MMOCoreORB/bin/scripts/mobile/lair/npc_theater/global_slicer_camp_neutral_small_theater.lua
@@ -6,6 +6,7 @@ global_slicer_camp_neutral_small_theater = Lair:new {
 	buildingsMedium = {"object/building/poi/tatooine_hutt_businessmen_camp_small1.iff","object/building/poi/tatooine_hutt_businessmen_camp_small2.iff"},
 	buildingsHard = {"object/building/poi/tatooine_hutt_businessmen_camp_small1.iff","object/building/poi/tatooine_hutt_businessmen_camp_small2.iff"},
 	buildingsVeryHard = {"object/building/poi/tatooine_hutt_businessmen_camp_small1.iff","object/building/poi/tatooine_hutt_businessmen_camp_small2.iff"},
+	missionBuilding = "object/tangible/lair/base/objective_banner_generic2.iff",
 	mobType = "npc",
 	buildingType = "theater"
 }

MMOCoreORB/bin/scripts/mobile/lair/npc_theater/global_thief_camp2_neutral_small_theater.lua
@@ -9,6 +9,7 @@ global_thief_camp2_neutral_small_theater = Lair:new {
 	buildingsMedium = {"object/building/poi/corellia_meatlump_small1.iff","object/building/poi/corellia_meatlump_small2.iff","object/building/poi/corellia_meatlump_small3.iff"},
 	buildingsHard = {"object/building/poi/corellia_meatlump_small1.iff","object/building/poi/corellia_meatlump_small2.iff","object/building/poi/corellia_meatlump_small3.iff"},
 	buildingsVeryHard = {"object/building/poi/corellia_meatlump_small1.iff","object/building/poi/corellia_meatlump_small2.iff","object/building/poi/corellia_meatlump_small3.iff"},
+	missionBuilding = "object/tangible/lair/base/objective_banner_generic2.iff",
 	mobType = "npc",
 	buildingType = "theater"
 }

MMOCoreORB/bin/scripts/mobile/lair/npc_theater/global_thief_camp_neutral_small_theater.lua
@@ -9,6 +9,7 @@ global_thief_camp_neutral_small_theater = Lair:new {
 	buildingsMedium = {"object/building/poi/corellia_meatlump_small1.iff","object/building/poi/corellia_meatlump_small2.iff","object/building/poi/corellia_meatlump_small3.iff"},
 	buildingsHard = {"object/building/poi/corellia_meatlump_small1.iff","object/building/poi/corellia_meatlump_small2.iff","object/building/poi/corellia_meatlump_small3.iff"},
 	buildingsVeryHard = {"object/building/poi/corellia_meatlump_small1.iff","object/building/poi/corellia_meatlump_small2.iff","object/building/poi/corellia_meatlump_small3.iff"},
+	missionBuilding = "object/tangible/lair/base/objective_banner_generic2.iff",
 	mobType = "npc",
 	buildingType = "theater"
 }

MMOCoreORB/bin/scripts/mobile/lair/npc_theater/global_wookiee_brawler_camp_neutral_small_theater.lua
@@ -6,6 +6,7 @@ global_wookiee_brawler_camp_neutral_small_theater = Lair:new {
 	buildingsMedium = {"object/building/poi/tatooine_rodianhunter_medium.iff"},
 	buildingsHard = {"object/building/poi/tatooine_rodianhunter_medium.iff"},
 	buildingsVeryHard = {"object/building/poi/tatooine_rodianhunter_medium.iff"},
+	missionBuilding = "object/tangible/lair/base/objective_banner_generic2.iff",
 	mobType = "npc",
 	buildingType = "theater"
 }

Tatwi/legend-of-hondo-classic

MMOCoreORB/bin/scripts/mobile/lair/npc_theater/global_bounty_hunter_camp_neutral_small_theater.lua
@@ -6,6 +6,7 @@ global_bounty_hunter_camp_neutral_small_theater = Lair:new {
 	buildingsMedium = {"object/building/poi/anywhere_misc_camp_small_1.iff"},
 	buildingsHard = {"object/building/poi/anywhere_misc_camp_small_1.iff"},
 	buildingsVeryHard = {"object/building/poi/anywhere_misc_camp_small_1.iff"},
+	missionBuilding = "object/tangible/lair/base/objective_banner_generic_2.iff",
 	mobType = "npc",
 	buildingType = "theater"
 }

MMOCoreORB/bin/scripts/mobile/lair/npc_theater/global_brigand_highwayman_camp_neutral_large_theater.lua
@@ -6,6 +6,7 @@ global_brigand_highwayman_camp_neutral_large_theater = Lair:new {
 	buildingsMedium =  {"object/building/poi/tatooine_swoop_large1.iff","object/building/poi/tatooine_swoop_large2.iff","object/building/poi/tatooine_swoop_large3.iff","object/building/poi/tatooine_swoop_large4.iff"},
 	buildingsHard =  {"object/building/poi/tatooine_swoop_large1.iff","object/building/poi/tatooine_swoop_large2.iff","object/building/poi/tatooine_swoop_large3.iff","object/building/poi/tatooine_swoop_large4.iff"},
 	buildingsVeryHard = {"object/building/poi/tatooine_swoop_large1.iff","object/building/poi/tatooine_swoop_large2.iff","object/building/poi/tatooine_swoop_large3.iff","object/building/poi/tatooine_swoop_large4.iff"},
+	missionBuilding = "object/tangible/lair/base/objective_banner_generic_2.iff",
 	mobType = "npc",
 	buildingType = "theater"
 }

MMOCoreORB/bin/scripts/mobile/lair/npc_theater/global_miner_camp_neutral_small_theater.lua
@@ -6,6 +6,7 @@ global_miner_camp_neutral_small_theater = Lair:new {
 	buildingsMedium = {"object/building/poi/tatooine_mineral_farmer_medium1.iff","object/building/poi/tatooine_mineral_farmer_medium2.iff"},
 	buildingsHard = {"object/building/poi/tatooine_mineral_farmer_medium1.iff","object/building/poi/tatooine_mineral_farmer_medium2.iff"},
 	buildingsVeryHard = {"object/building/poi/tatooine_mineral_farmer_medium1.iff","object/building/poi/tatooine_mineral_farmer_medium2.iff"},
+	missionBuilding = "object/tangible/lair/base/objective_banner_generic_2.iff",
 	mobType = "npc",
 	buildingType = "theater"
 }

MMOCoreORB/bin/scripts/mobile/lair/npc_theater/global_slicer_camp_neutral_small_theater.lua
@@ -6,6 +6,7 @@ global_slicer_camp_neutral_small_theater = Lair:new {
 	buildingsMedium = {"object/building/poi/tatooine_hutt_businessmen_camp_small1.iff","object/building/poi/tatooine_hutt_businessmen_camp_small2.iff"},
 	buildingsHard = {"object/building/poi/tatooine_hutt_businessmen_camp_small1.iff","object/building/poi/tatooine_hutt_businessmen_camp_small2.iff"},
 	buildingsVeryHard = {"object/building/poi/tatooine_hutt_businessmen_camp_small1.iff","object/building/poi/tatooine_hutt_businessmen_camp_small2.iff"},
+	missionBuilding = "object/tangible/lair/base/objective_banner_generic_2.iff",
 	mobType = "npc",
 	buildingType = "theater"
 }

MMOCoreORB/bin/scripts/mobile/lair/npc_theater/global_thief_camp2_neutral_small_theater.lua
@@ -9,6 +9,7 @@ global_thief_camp2_neutral_small_theater = Lair:new {
 	buildingsMedium = {"object/building/poi/corellia_meatlump_small1.iff","object/building/poi/corellia_meatlump_small2.iff","object/building/poi/corellia_meatlump_small3.iff"},
 	buildingsHard = {"object/building/poi/corellia_meatlump_small1.iff","object/building/poi/corellia_meatlump_small2.iff","object/building/poi/corellia_meatlump_small3.iff"},
 	buildingsVeryHard = {"object/building/poi/corellia_meatlump_small1.iff","object/building/poi/corellia_meatlump_small2.iff","object/building/poi/corellia_meatlump_small3.iff"},
+	missionBuilding = "object/tangible/lair/base/objective_banner_generic_2.iff",
 	mobType = "npc",
 	buildingType = "theater"
 }

MMOCoreORB/bin/scripts/mobile/lair/npc_theater/global_thief_camp_neutral_small_theater.lua
@@ -9,6 +9,7 @@ global_thief_camp_neutral_small_theater = Lair:new {
 	buildingsMedium = {"object/building/poi/corellia_meatlump_small1.iff","object/building/poi/corellia_meatlump_small2.iff","object/building/poi/corellia_meatlump_small3.iff"},
 	buildingsHard = {"object/building/poi/corellia_meatlump_small1.iff","object/building/poi/corellia_meatlump_small2.iff","object/building/poi/corellia_meatlump_small3.iff"},
 	buildingsVeryHard = {"object/building/poi/corellia_meatlump_small1.iff","object/building/poi/corellia_meatlump_small2.iff","object/building/poi/corellia_meatlump_small3.iff"},
+	missionBuilding = "object/tangible/lair/base/objective_banner_generic_2.iff",
 	mobType = "npc",
 	buildingType = "theater"
 }

MMOCoreORB/bin/scripts/mobile/lair/npc_theater/global_wookiee_brawler_camp_neutral_small_theater.lua
@@ -6,6 +6,7 @@ global_wookiee_brawler_camp_neutral_small_theater = Lair:new {
 	buildingsMedium = {"object/building/poi/tatooine_rodianhunter_medium.iff"},
 	buildingsHard = {"object/building/poi/tatooine_rodianhunter_medium.iff"},
 	buildingsVeryHard = {"object/building/poi/tatooine_rodianhunter_medium.iff"},
+	missionBuilding = "object/tangible/lair/base/objective_banner_generic_2.iff",
 	mobType = "npc",
 	buildingType = "theater"
 }