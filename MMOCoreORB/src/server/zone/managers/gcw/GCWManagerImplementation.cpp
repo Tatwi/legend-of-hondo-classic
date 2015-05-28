swgemu/Core3

Modify GetGCWDiscount() : the function was only able to set the GCW Discount as a penality. (only negative Values from the Lua) Add two new Values looserBonus and winnerBonus. Both can have positive or negative values to give either a Bonus or a Penalty The setting here was that the looserside get a panalty of 30% We set it on our server as a winnerBonus 30 and a looserbonus 0 Iam not sure but i think that was on pre cu equal. But i set the config you have had as a looserpenalty of 30%
Change-Id: I553527565c8e1de3f5cb45d6c891f21d5626f66e

MMOCoreORB/src/server/zone/managers/gcw/GCWManagerImplementation.cpp
@@ -69,7 +69,8 @@ int GCWManagerImplementation::reactvationTimer = 300;
 int GCWManagerImplementation::turretAutoFireTimeout = 120;
 int GCWManagerImplementation::maxBasesPerPlayer = 3;
 int GCWManagerImplementation::bonusXP = 15;
-int GCWManagerImplementation::bonusDiscount = 30;
+int GCWManagerImplementation::looserBonus = 0;
+int GCWManagerImplementation::winnerBonus = 30;
 bool GCWManagerImplementation::racialPenaltyEnabled = true;
 bool GCWManagerImplementation::spawnDefenses = true;
 int GCWManagerImplementation::initialVulnerabilityDelay = 0;
@@ -132,7 +133,8 @@ void GCWManagerImplementation::loadLuaConfig(){
 	turretAutoFireTimeout = lua->getGlobalInt("turretAutoFireTimeout");
 	maxBasesPerPlayer = lua->getGlobalInt("maxBasesPerPlayer");
 	bonusXP = lua->getGlobalInt("bonusXP");
-	bonusDiscount = lua->getGlobalInt("bonusDiscount");
+	winnerBonus = lua->getGlobalInt("winnerBonus");
+	looserBonus = lua->getGlobalInt("looserBonus");
 	racialPenaltyEnabled = lua->getGlobalInt("racialPenaltyEnabled");
 	initialVulnerabilityDelay = lua->getGlobalInt("initialVulnerabilityDelay");
 	spawnDefenses = lua->getGlobalInt("spawnDefenses");
@@ -2626,13 +2628,18 @@ void GCWManagerImplementation::awardSlicingXP(CreatureObject* creature,  const S
 
 
 // returns a cost multiplier for faction items
-// includes racial penalty and gcwdiscount
+// includes racial penalty and Bonus&Penality for Looser and Winner side
 float GCWManagerImplementation::getGCWDiscount(CreatureObject* creature){
 
 	float discount = 1.0f;
 
-	if(getWinningFaction() != 1 && getWinningFaction() != creature->getFaction() && creature->getFaction() != 0)
-		 discount -= bonusDiscount/100.f;
+  	if (getWinningFaction() != 1 && creature->getFaction() != 0) {
+  		if (getWinningFaction() == creature->getFaction()){
+  			discount -= winnerBonus /100.f;
+  		}else {
+  			discount -= looserBonus /100.f;
+  		}
+  	}
 
 	if(creature->getFaction() == IMPERIALHASH && racialPenaltyEnabled && getRacialPenalty(creature->getSpecies()) > 0)
 		discount *= getRacialPenalty(creature->getSpecies());

Tatwi/legend-of-hondo-classic

MMOCoreORB/src/server/zone/managers/gcw/GCWManagerImplementation.cpp
@@ -69,7 +69,8 @@ int GCWManagerImplementation::reactvationTimer = 300;
 int GCWManagerImplementation::turretAutoFireTimeout = 120;
 int GCWManagerImplementation::maxBasesPerPlayer = 3;
 int GCWManagerImplementation::bonusXP = 15;
-int GCWManagerImplementation::bonusDiscount = 30;
+int GCWManagerImplementation::loserBonus = 0;
+int GCWManagerImplementation::winnerBonus = 30;
 bool GCWManagerImplementation::racialPenaltyEnabled = true;
 bool GCWManagerImplementation::spawnDefenses = true;
 int GCWManagerImplementation::initialVulnerabilityDelay = 0;
@@ -132,7 +133,8 @@ void GCWManagerImplementation::loadLuaConfig(){
 	turretAutoFireTimeout = lua->getGlobalInt("turretAutoFireTimeout");
 	maxBasesPerPlayer = lua->getGlobalInt("maxBasesPerPlayer");
 	bonusXP = lua->getGlobalInt("bonusXP");
-	bonusDiscount = lua->getGlobalInt("bonusDiscount");
+	winnerBonus = lua->getGlobalInt("winnerBonus");
+	loserBonus = lua->getGlobalInt("loserBonus");
 	racialPenaltyEnabled = lua->getGlobalInt("racialPenaltyEnabled");
 	initialVulnerabilityDelay = lua->getGlobalInt("initialVulnerabilityDelay");
 	spawnDefenses = lua->getGlobalInt("spawnDefenses");
@@ -2626,13 +2628,18 @@ void GCWManagerImplementation::awardSlicingXP(CreatureObject* creature,  const S
 
 
 // returns a cost multiplier for faction items
-// includes racial penalty and gcwdiscount
+// includes racial penalty and Bonus&Penality for Loser and Winner side
 float GCWManagerImplementation::getGCWDiscount(CreatureObject* creature){
 
 	float discount = 1.0f;
 
-	if(getWinningFaction() != 1 && getWinningFaction() != creature->getFaction() && creature->getFaction() != 0)
-		 discount -= bonusDiscount/100.f;
+  	if (getWinningFaction() != 1 && creature->getFaction() != 0) {
+  		if (getWinningFaction() == creature->getFaction()){
+  			discount -= winnerBonus /100.f;
+  		}else {
+  			discount -= loserBonus /100.f;
+  		}
+  	}
 
 	if(creature->getFaction() == IMPERIALHASH && racialPenaltyEnabled && getRacialPenalty(creature->getSpecies()) > 0)
 		discount *= getRacialPenalty(creature->getSpecies());