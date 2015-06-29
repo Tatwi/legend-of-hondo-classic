040df7e2caca2a351fd4fa86a822e8eec8efcd19
Tatwi <rbassettjr@tpot.ca>
2015-06-29 02:55:55 -0400

    Feature: Skill/XP Terminal. See LoH Feature Log for more details.

--- MMOCoreORB/bin/scripts/object/tangible/beta/beta_terminal_xp.lua
+++ MMOCoreORB/bin/scripts/object/tangible/beta/beta_terminal_xp.lua
@@ -43,6 +43,216 @@
 
 object_tangible_beta_beta_terminal_xp = object_tangible_beta_shared_beta_terminal_xp:new {
 
+	gameObjectType = 16400,
+			
+	maxCondition = 0,
+
+	templateType = CHARACTERBUILDERTERMINAL,
+
+	itemList = {
+		"Learn Skill",
+		{
+			"Dancer",
+			{
+				"Novice", "social_dancer_novice",
+				"Techniques",
+				{
+					"Dancing Techniques I", "social_dancer_ability_01",
+					"Dancing Techniques II", "social_dancer_ability_02",
+					"Dancing Techniques III", "social_dancer_ability_03",
+					"Technique Specialist IV", "social_dancer_ability_04"
+				},
+				"Wound Healing",
+				{
+					"Dancer's Wound Healing I", "social_dancer_wound_01",
+					"Dancer's Wound Healing II", "social_dancer_wound_02",
+					"Dancer's Wound Healing III", "social_dancer_wound_03",
+					"Dancer's Wound Healing IV", "social_dancer_wound_04"
+				},
+				"Knowledge",
+				{
+					"Dancing Knowledge I", "social_dancer_knowledge_01",
+					"Dancing Knowledge II", "social_dancer_knowledge_02",
+					"Dancing Knowledge III", "social_dancer_knowledge_03",
+					"Dancing Knowledge IV", "social_dancer_knowledge_04"
+				},
+				"Fatigue Healing",
+				{
+					"Dancer's Fatigue Healing I", "social_dancer_shock_01",
+					"Dancer's Fatigue Healing II", "social_dancer_shock_02",
+					"Dancer's Fatigue Healing III", "social_dancer_shock_03",
+					"Dancer's Fatigue Healing IV", "social_dancer_shock_04"
+				},
+				"Master", "social_dancer_master"
+			},
+			"Entertainer",
+			{
+				"Novice", "social_entertainer_novice",
+				"Image Design",
+				{
+					"Image Design I", "social_entertainer_hairstyle_01",
+					"Image Design II", "social_entertainer_hairstyle_02",
+					"Image Design III", "social_entertainer_hairstyle_03",
+					"Image Design IV", "social_entertainer_hairstyle_04"
+				},
+				"Musicianship",
+				{
+					"Musicianship I", "social_entertainer_music_01",
+					"Musicianship II", "social_entertainer_music_02",
+					"Musicianship III", "social_entertainer_music_03",
+					"Musicianship IV", "social_entertainer_music_04"
+				},
+				"Dancing",
+				{
+					"Dancing I", "social_entertainer_dance_01",
+					"Dancing II", "social_entertainer_dance_02",
+					"Dancing III", "social_entertainer_dance_03",
+					"Dancing IV", "social_entertainer_dance_04"
+				},
+				"Entertainment Healing",
+				{
+					"Entertainment Healing I", "social_entertainer_healing_01",
+					"Entertainment Healing II", "social_entertainer_healing_02",
+					"Entertainment Healing III", "social_entertainer_healing_03",
+					"Entertainment Healing IV", "social_entertainer_healing_04"
+				},
+				"Master", "social_entertainer_master"
+			},
+			"Image Designer",
+			{
+				"Novice", "social_imagedesigner_novice",
+				"Hairstyling",
+				{
+					"Hairstyling I", "social_imagedesigner_hairstyle_01",
+					"Hairstyling II", "social_imagedesigner_hairstyle_02",
+					"Hairstyling III", "social_imagedesigner_hairstyle_03",
+					"Hairstyling IV", "social_imagedesigner_hairstyle_04"
+				},
+				"Face",
+				{
+					"Face I", "social_imagedesigner_exotic_01",
+					"Face II", "social_imagedesigner_exotic_02",
+					"Face III", "social_imagedesigner_exotic_03",
+					"Face IV", "social_imagedesigner_exotic_04"
+				},
+				"Bodyform",
+				{
+					"Bodyform I", "social_imagedesigner_bodyform_01",
+					"Bodyform II", "social_imagedesigner_bodyform_02",
+					"Bodyform III", "social_imagedesigner_bodyform_03",
+					"Bodyform Iv", "social_imagedesigner_bodyform_04"
+				},
+				"Markings",
+				{
+					"Markings I", "social_imagedesigner_markings_01",
+					"Markings II", "social_imagedesigner_markings_02",
+					"Markings III", "social_imagedesigner_markings_03",
+					"Markings IV", "social_imagedesigner_markings_04"
+				},
+				"Master", "social_imagedesigner_master"
+			},
+			"Merchant",
+			{
+				"Novice", "crafting_merchant_novice",
+				"Advertising",
+				{
+					"Advertising I", "crafting_merchant_advertising_01",
+					"Advertising II", "crafting_merchant_advertising_02",
+					"Advertising III", "crafting_merchant_advertising_03",
+					"Advertising IV", "crafting_merchant_advertising_04"
+				},
+				"Efficiency",
+				{
+					"Efficiency I", "crafting_merchant_sales_01",
+					"Efficiency II", "crafting_merchant_sales_02",
+					"Efficiency III", "crafting_merchant_sales_03",
+					"Efficiency IV", "crafting_merchant_sales_04"
+				},
+				"Hiring",
+				{
+					"Hiring I", "crafting_merchant_hiring_01",
+					"Hiring II", "crafting_merchant_hiring_02",
+					"Hiring III", "crafting_merchant_hiring_03",
+					"Hiring IV", "crafting_merchant_hiring_04"
+				},
+				"Management",
+				{
+					"Management I", "crafting_merchant_management_01",
+					"Management II", "crafting_merchant_management_02",
+					"Management III", "crafting_merchant_management_03",
+					"Management IV", "crafting_merchant_management_04"
+				},
+				"Master", "crafting_merchant_master"
+			},
+			"Musician",
+			{
+				"Novice", "social_musician_novice",
+				"Techniques",
+				{
+					"Musical Techniques I", "social_musician_ability_01",
+					"Musical Techniques II", "social_musician_ability_02",
+					"Musical Techniques III", "social_musician_ability_03",
+					"Technique Specialist IV", "social_musician_ability_04"
+				},
+				"Wound Healing",
+				{
+					"Musician's Wound Healing I", "social_musician_wound_01",
+					"Musician's Wound Healing II", "social_musician_wound_02",
+					"Musician's Wound Healing III", "social_musician_wound_03",
+					"Musician's Wound Healing IV", "social_musician_wound_04"
+				},
+				"Knowledge",
+				{
+					"Musical Knowledge I", "social_musician_knowledge_01",
+					"Musical Knowledge II", "social_musician_knowledge_02",
+					"Musical Knowledge III", "social_musician_knowledge_03",
+					"Musical Knowledge IV", "social_musician_knowledge_04"
+				},
+				"Fatigue Healing",
+				{
+					"Musician's Fatigue Healing I", "social_musician_shock_01",
+					"Musician's Fatigue Healing II", "social_musician_shock_02",
+					"Musician's Fatigue Healing III", "social_musician_shock_03",
+					"Musician's Fatigue Healing IV", "social_musician_shock_04"
+				},
+				"Master", "social_musician_master"
+			},
+			"Politician",
+			{
+				"Novice", "social_politician_novice",
+				"Fiscal Policy",
+				{
+					"Fiscal Policy I", "social_politician_fiscal_01",
+					"Fiscal Policy II", "social_politician_fiscal_02",
+					"Fiscal Policy III", "social_politician_fiscal_03",
+					"Fiscal Policy IV", "social_politician_fiscal_04"
+				},
+				"Martial Policy",
+				{
+					"Martial Policy I", "social_politician_martial_01",
+					"Martial Policy II", "social_politician_martial_02",
+					"Martial Policy III", "social_politician_martial_03",
+					"Martial Policy IV", "social_politician_martial_04"
+				},
+				"Civic Policy",
+				{
+					"Civic Policy I", "social_politician_civic_01",
+					"Civic Policy II", "social_politician_civic_02",
+					"Civic Policy III", "social_politician_civic_03",
+					"Civic Policy IV", "social_politician_civic_04"
+				},
+				"City Customization",
+				{
+					"City Customization I", "social_politician_urban_01",
+					"City Customization II", "social_politician_urban_02",
+					"City Customization III", "social_politician_urban_03",
+					"City Customization IV", "social_politician_urban_04"
+				},
+				"Master", "social_politician_master"
+			},
+		},
+		"Learn Languages", "language",
+	}
 }
 
 ObjectTemplates:addTemplate(object_tangible_beta_beta_terminal_xp, "object/tangible/beta/beta_terminal_xp.iff")
--- MMOCoreORB/bin/scripts/screenplays/hondo/buff_terminals.lua
+++ MMOCoreORB/bin/scripts/screenplays/hondo/buff_terminals.lua
@@ -17,9 +17,17 @@ end
 
 
 function BuffTerminals:spawnSceneObjects()
-	-- Tatooine
-	spawnSceneObject("tatooine", "object/tangible/beta/beta_terminal_wound.iff", 1.1233, 0.184067, 0.0367278, 4005383, 0.790803, 0, 0.61207, 0) -- Bestine
-	spawnSceneObject("tatooine", "object/tangible/beta/beta_terminal_wound.iff", 1.1233, 0.184067, 0.0367278, 9655496, 0.790803, 0, 0.61207, 0) -- Mos Eisley
-	spawnSceneObject("tatooine", "object/tangible/beta/beta_terminal_wound.iff", -21.7355, 0.26, -2.34, 1153586, 0.999413, 0, 0.0342538, 0) -- Mos Entha
-	spawnSceneObject("tatooine", "object/tangible/beta/beta_terminal_wound.iff", 1.1233, 0.184067, 0.0367278, 4005424, 0.790803, 0, 0.61207, 0) -- Mos Espa
+
+	-- Bestine
+	spawnSceneObject("tatooine", "object/tangible/beta/beta_terminal_wound.iff", 1.1233, 0.184067, 0.0367278, 4005383, 0.790803, 0, 0.61207, 0) 
+	-- Mos Eisley
+	spawnSceneObject("tatooine", "object/tangible/beta/beta_terminal_wound.iff", 1.1233, 0.184067, 0.0367278, 9655496, 0.790803, 0, 0.61207, 0) 
+	 -- Mos Entha
+	spawnSceneObject("tatooine", "object/tangible/beta/beta_terminal_wound.iff", -21.7355, 0.26, -2.34, 1153586, 0.999413, 0, 0.0342538, 0)
+	-- Mos Espa
+	spawnSceneObject("tatooine", "object/tangible/beta/beta_terminal_wound.iff", 1.1233, 0.184067, 0.0367278, 4005424, 0.790803, 0, 0.61207, 0) 
+
+	-- XP Termina, Mos Espa Cantina
+	spawnSceneObject("tatooine", "object/tangible/beta/beta_terminal_xp.iff",  27.0829, -0.894995, 8.00572, 1256058,  -0.704913, 0, 0.709294, 0) 
+
 end
