ec9bf702a2398ff375a04bc704423f72d9e54507
Tatwi <rbassettjr@tpot.ca>
2015-07-01 12:28:15 -0400

    Fixes: Lair spawn tuning. Players can migrate their stats anywhere on Tatooine, by themselves.

--- MMOCoreORB/src/server/zone/objects/creature/commands/RequestSetStatMigrationDataCommand.h
+++ MMOCoreORB/src/server/zone/objects/creature/commands/RequestSetStatMigrationDataCommand.h
@@ -87,7 +87,7 @@ public:
 		//Player is in the tutorial zone and is allowed to migrate stats.
 		Zone* zone = creature->getZone();
 
-		if (zone != NULL && zone->getZoneName() == "tutorial")
+		if (zone != NULL && (zone->getZoneName() == "tutorial" or "tatooine"))
 			session->migrateStats();
 
 
