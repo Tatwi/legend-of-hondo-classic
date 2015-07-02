663faa8a818a8fce7a6f00092516b3c3a18c0ff7
Tatwi <rbassettjr@tpot.ca>
2015-07-02 00:21:46 -0400

    Fist steps in new merchant system. Goods from list and checking friend/foe status.

---
+++ MMOCoreORB/bin/scripts/mobile/hondo/merchants/testosio.lua
@@ -0,0 +1,38 @@
+testosio = Creature:new {
+	objectName = "@mob/creature_names:commoner",
+	customName = "Testosio (Merchant)",
+	socialGroup = "townsperson",
+	pvpFaction = "townsperson",
+	faction = "townsperson",
+	level = 1,
+	chanceHit = 0.25,
+	damageMin = 50,
+	damageMax = 55,
+	baseXp = 113,
+	baseHAM = 180,
+	baseHAMmax = 220,
+	armor = 0,
+	resists = {0,0,0,0,0,0,0,-1,-1},
+	meatType = "",
+	meatAmount = 0,
+	hideType = "",
+	hideAmount = 0,
+	boneType = "",
+	boneAmount = 0,
+	milk = 0,
+	tamingChance = 0,
+	ferocity = 0,
+	pvpBitmask = NONE,
+	creatureBitmask = NONE,
+	optionsBitmask = 264, --for conversation
+	diet = HERBIVORE,
+
+	templates = {"object/mobile/r2.iff"},
+	lootGroups = {},
+	weapons = {},
+	conversationTemplate = "testosio_template",
+	attacks = {
+	}
+}
+
+CreatureTemplates:addCreatureTemplate(testosio, "testosio")
