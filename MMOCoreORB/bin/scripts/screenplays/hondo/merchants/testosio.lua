212cc5d26216ed177ea027e7b74fc01462dd56f5
Tatwi <rbassettjr@tpot.ca>
2015-07-06 03:13:13 -0400

    Added price adjustment functionality.

--- MMOCoreORB/bin/scripts/screenplays/hondo/merchants/testosio.lua
+++ MMOCoreORB/bin/scripts/screenplays/hondo/merchants/testosio.lua
@@ -12,16 +12,17 @@ TestosioSP = ScreenPlay:new {
 		-- NPC's standing with these factions. 
 		-- Player must have higher npcStanding to buy. 
 		-- Negative number means enemy faction, lower values make NPC more leinient toward player relations with his enemy. 
-		-- Set to -1 to make NPC refuse to talk if player has 2+ faction with his enemy.
+		-- Set npcStanding=-1 to make NPC refuse to talk if player has 2+ faction with his enemy.
+		-- Set npcStanding="sympathetic" to make the NPC always do business, but also adjust the price based on the specified faction standing.
 		-- NIL faction values mean player always accepted, no discount, no sircharge.
-		-- priceAdjust applies discounts and sircharges on a sliding scale.
+		-- priceAdjust applies % discount/sircharge on a sliding scale. Use 1 to 99. Effects are cumulative when multiple factions are present.
 		{name="jabba", npcStanding=300, priceAdjust=25}, -- Friend
-		{name="rebel", npcStanding=0, priceAdjust=10}, -- Friend
+		{name="rebel", npcStanding="sympathetic", priceAdjust=10}, -- Adjust price only
 		{name="thug", npcStanding=-1000, priceAdjust=50}  -- Enemy
 	},
 	goods = {
-		{optName="survey_mineral", cost=5, itemName="Mineral Survey Tool", item="object/tangible/survey_tool/survey_tool_mineral.iff"}, 
-		{optName="survey_chemical", cost=10, itemName="Chemical Survey Tool", item="object/tangible/survey_tool/survey_tool_liquid.iff"},
+		{optName="survey_mineral", cost=250, itemName="Mineral Survey Tool", item="object/tangible/survey_tool/survey_tool_mineral.iff"}, 
+		{optName="survey_chemical", cost=300, itemName="Chemical Survey Tool", item="object/tangible/survey_tool/survey_tool_liquid.iff"},
 		{optName="slitherhorn", cost=100, itemName="Slitherhorn", item="object/tangible/instrument/slitherhorn.iff"}, 
 		{optName="fanfar", cost=500, itemName="Fanfar", item="object/tangible/instrument/fanfar.iff"}
 	},
@@ -46,35 +47,46 @@ end
 function TestosioSP:refuseService(conversingPlayer)
 	return ObjectManager.withCreatureAndPlayerObject(conversingPlayer, function(creatureObject, playerObject)
 		-- Check NPC/Player relations
-		local breaker = "false"
 		for lc = 1, table.getn(TestosioSP.relations) , 1 do
 			local playerStanding = playerObject:getFactionStanding(TestosioSP.relations[lc].name)
 			
-			if (playerStanding < TestosioSP.relations[lc].npcStanding) then
+			if (TestosioSP.relations[lc].npcStanding == "sympathetic") then
+				return 0 -- Will talk to player and adjust price based on faction
+			elseif (playerStanding < TestosioSP.relations[lc].npcStanding) then
 				creatureObject:sendSystemMessage("Hint: Increase your " .. TestosioSP:firstToUpper(TestosioSP.relations[lc].name) .. " faction and speak with the NPC again.")
-				breaker = "true"
 				return 2 -- Not friendly enough for service
 			elseif (TestosioSP.relations[lc].npcStanding < 0 and (TestosioSP.relations[lc].npcStanding + playerStanding) > 0) then
 				creatureObject:sendSystemMessage("Hint: Decrease your " .. TestosioSP:firstToUpper(TestosioSP.relations[lc].name) .. " faction and speak with the NPC again.")
-				breaker = "true"
 				return 1 -- Too friendly with my enemy for service
 			end	
-			
-			if (breaker == "true") then 
-				break
-			end
 		end
 		return 0 -- Will talk to player
 	end)
 end
 
-
-
-
-
-
-
-
+function TestosioSP:adjustPrice(cost)
+	return ObjectManager.withCreatureAndPlayerObject(conversingPlayer, function(creatureObject, playerObject)
+		local priceAdjustment = 0
+		local finalPrice = cost
+		
+		-- Calculate price adjustment
+		for lc = 1, table.getn(TestosioSP.relations) , 1 do
+			local playerStanding = playerObject:getFactionStanding(TestosioSP.relations[lc].name)
+			local adjustment = TestosioSP.goods[lc].priceAdjust
+			
+			if (playerStanding ~= nil) then
+				if (playerStanding > 0) then
+					-- Discount
+					finalPrice = finalPrice / ((adjustment * playerStanding / 5000 ) / 100 + 1)
+				elseif (playerStanding < 0) then
+					-- Surcharge
+					finalPrice = finalPrice * ((adjustment * abs(playerStanding) / 5000 ) / 100 + 1)
+				end	
+			end
+		end
+		return math.floor(finalPrice)
+	end)
+end
 
 -- Conversation Logic
 
@@ -108,11 +120,12 @@ function testosio_convo_handler:getNextConversationScreen(conversationTemplate, 
 		end
 		
 		local insufficientFunds = "false"
-		
-		-- See if NPC will talk to player
-		local canTalk = TestosioSP:refuseService(conversingPlayer)
+		local noSpace = "false"
 		
 		if ( lastConversationScreen == nil ) then
+			-- See if NPC will talk to player
+			local canTalk = TestosioSP:refuseService(conversingPlayer)
+		
 			if (canTalk == 1) then
 				nextConversationScreen = conversation:getScreen("get_lost")
 			elseif (canTalk == 2) then
@@ -128,14 +141,24 @@ function testosio_convo_handler:getNextConversationScreen(conversationTemplate, 
 			-- Sell Item
 			for lc = 1, table.getn(TestosioSP.goods) , 1 do
 				if (optionLink == TestosioSP.goods[lc].optName) then 
-					creature:subtractCashCredits(TestosioSP.goods[lc].cost)
-					local pItem = giveItem(pInventory, TestosioSP.goods[lc].item, -1)
+					if (inventory:hasFullContainerObjects() == true) then
+						-- Bail if the player doesn't have enough space in their inventory.
+						noSpace = "true"
+						creature:sendSystemMessage("You need 1 available inventory space to complete transaction.")
+					else
+						-- Sell the item
+						local chargePlayer = TestosioSP:adjustPrice(TestosioSP.goods[lc].cost)
+						creature:subtractCashCredits(chargePlayer)
+						local pItem = giveItem(pInventory, TestosioSP.goods[lc].item, -1)
+					end
 				end
 			end
 			
 			-- Play the next screen or jump to insufficient_funds
 			if (insufficientFunds == "true") then
 				nextConversationScreen = conversation:getScreen("insufficient_funds")
+			elseif (noSpace == "true") then
+				nextConversationScreen = conversation:getScreen("no_space")
 			else
 				nextConversationScreen = conversation:getScreen(optionLink)
 			end	
